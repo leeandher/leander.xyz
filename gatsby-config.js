2f9dca4ae046a2e1498d226a33cfc4088c589c5e
Leander Rodrigues <leandergrodrigues@gmail.com>
2018-12-18 20:09:13 -0500

    setting up template blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`]
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog`,
+        name: "Markdown-Posts"
+      }
+    },
+    `gatsby-transformer-remark`
+  ]
 };
