9f21bfad8bd373fa510f49bcc303b7c8c335304f
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-01-21 21:59:26 -0500

    :construction: Reorganize structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,19 @@
 module.exports = {
   plugins: [
+    // CMS
     `gatsby-plugin-netlify-cms`,
+
+    // Styling,
+    `gatsby-plugin-styled-components`,
+
+    // Blog
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/blog`,
         name: "Markdown-Posts"
       }
-    },
-    `gatsby-transformer-remark`
+    }
   ]
 };
