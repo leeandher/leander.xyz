3c3e01e42bef4680e01644d17e293239451585bd
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-01-24 17:50:15 -0500

    🔧 Prettier config setup

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Particles from 'react-particles-js'
+
+const Home = () => {
+  return (
+    <Particles>
+      <div>Hello workd!</div>
+    </Particles>
+  )
+}
+
+export default Home
