89aff2362c0a1e1c6c7cbd61b69b310f96f84df5
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-02-16 12:40:57 -0500

    🎨 Setup palette

---
+++ src/styles/palette.js
@@ -0,0 +1,19 @@
+export default {
+  color: {
+    red: ' #ce2d48',
+    orange: '#ff9f1c',
+    yellow: '#f1bc51',
+    green: '#7ae582',
+    teal: '#00cccc',
+    blue: '#0582ca',
+    purple: '#b65fd1',
+    pink: '#d33797',
+  },
+  shade: {
+    darker: '#303030',
+    dark: '#686868',
+    mid: '#d8d8d8',
+    light: '#f2f2f2',
+    lighter: '#fefefe',
+  },
+}
