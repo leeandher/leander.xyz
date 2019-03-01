8270abdecde795d6da307050ba7337ab8637212c
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-03-01 11:55:09 -0500

    🚚 Page file changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,19 @@
-export { default } from './Home'
+import React from 'react'
+import styled from 'styled-components'
+
+import PageTitle from '../components/PageTitle'
+import PageHeader from '../components/PageHeader'
+
+import palette from '../styles'
+
+const Home = () => {
+  return (
+    <>
+      <PageHeader design="bubbles" height="100vh">
+        <PageTitle title="Hey there" />
+      </PageHeader>
+    </>
+  )
+}
+
+export default Home
