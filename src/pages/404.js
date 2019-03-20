2b688d678506797a6e8acc6bcf400954f8725351
Leander Rodrigues <me@leander.xyz>
2019-03-20 15:06:38 -0400

    🤡 Mocked up other pages than index

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,39 @@
 import React from "react"
+import styled from "styled-components"
 
-export default () => <div>Error!</div>
+import Page from "../components/Page"
+
+const Header = styled.div`
+  height: 100vh;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: ${({ theme }) => theme.spacing.default};
+  color: ${({ theme }) => theme.accent};
+  span {
+    color: ${({ theme }) => theme.shade.lighter};
+  }
+`
+
+const Projects = () => {
+  return (
+    <Page
+      accent="pink"
+      accentBg
+      title="I think we're lost"
+      design="mesh"
+      description="Hi there! I'm glad you've stumbled across my humble personal site. I have a bunch of projects, notes, blog posts, and even a snazzy resume for you to see!"
+    >
+      <Header>
+        <h1>
+          <code>
+            //TODO: <span>Make better 404 page</span>
+          </code>
+        </h1>
+      </Header>
+    </Page>
+  )
+}
+
+export default Projects
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,3 +1,39 @@
 import React from "react"
+import styled from "styled-components"
 
-export default () => <div style={{ background: "white" }}>Hello blog!</div>
+import Page from "../components/Page"
+
+const Header = styled.div`
+  height: 100vh;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: ${({ theme }) => theme.spacing.default};
+  color: ${({ theme }) => theme.accent};
+  span {
+    color: ${({ theme }) => theme.shade.lighter};
+  }
+`
+
+const Blog = () => {
+  return (
+    <Page
+      accent="yellow"
+      accentBg
+      title="Writing is hard"
+      design="bubbles"
+      description="Hi there! I'm glad you've stumbled across my humble personal site. I have a bunch of projects, notes, blog posts, and even a snazzy resume for you to see!"
+    >
+      <Header>
+        <h1>
+          <code>
+            //TODO: <span>Make blog page</span>
+          </code>
+        </h1>
+      </Header>
+    </Page>
+  )
+}
+
+export default Blog
