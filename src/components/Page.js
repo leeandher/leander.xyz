24353156204fe0b91a926dcffe9544d9ee73b9a7
Leander Rodrigues <me@leander.xyz>
2019-04-01 13:40:39 -0400

    🍱 Add preliminary iconography

--- src/components/Page.js
+++ src/components/Page.js
@@ -83,7 +83,7 @@ const StyledPage = styled.div`
   * {
     mark,
     ::selection {
-      background: ${({ theme }) => theme.accent}88;
+      background: ${({ theme }) => theme.accent}80;
     }
   }
 `
@@ -121,6 +121,31 @@ class Page extends React.Component {
         <Helmet>
           <title>{title}</title>
           <meta name="description" content={description} />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/safari-pinned-tab.svg"
+            color={theme.accent}
+          />
+          <meta name="msapplication-TileColor" content={theme.accent} />
+          <meta name="theme-color" content={theme.shade.darker} />
         </Helmet>
         <ThemeProvider theme={theme}>
           <StyledPage>
