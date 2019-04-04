944ed5fdc9888316b01e31923e15d3741d345389
Leander Rodrigues <me@leander.xyz>
2019-04-04 16:57:05 -0400

    🙈 Remove subdomains from gitignore

--- subdomains/stacker-news/src/constants.js
+++ subdomains/stacker-news/src/constants.js
@@ -1,4 +1,4 @@
 // FRONTEND SECRETS
-export const AUTH_TOKEN = "4ur3y350nly";
-export const LINKS_PER_PAGE = 15;
-export const GRAPHQL_ENDPOINT = "localhost:4000";
+export const AUTH_TOKEN = "4ur3y350nly"
+export const LINKS_PER_PAGE = 15
+export const GRAPHQL_ENDPOINT = "stacker-news.herokuapp.com/"
