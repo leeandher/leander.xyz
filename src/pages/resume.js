98334c8a0ef813ac67aa5c9067634406d9252a58
Leander Rodrigues <me@leander.xyz>
2019-05-12 11:49:01 -0400

    🚧 Work on resume navigation

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,6 +1,6 @@
 import React from "react"
 import styled from "styled-components"
-import { animated } from "react-spring"
+import { useSpring, animated } from "react-spring"
 
 import BusinessCard from "../components/BusinessCard"
 import Page from "../components/Page"
@@ -9,7 +9,7 @@ import { Default } from "../components/PageSections"
 
 import resume from "../data/resume.json"
 
-import { themer } from "../styles/helpers"
+import { media, themer } from "../styles/helpers"
 
 const Header = styled.div`
   padding-top: 100px;
@@ -22,7 +22,6 @@ const MeCard = styled(BusinessCard)`
 
 const ResumeSection = styled(Default)`
   margin: 2rem 0;
-  border: 2px solid red;
 `
 
 const ResumeLayout = styled(animated.div)`
@@ -47,22 +46,81 @@ const ResumeLayout = styled(animated.div)`
   }
 `
 
+const PageWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 150px auto;
+`
+
+const Scroller = styled.aside`
+  position: sticky;
+  color: ${themer("shade.lightest")};
+  top: ${themer("constants.navBarHeight")};
+  button {
+    padding: 2rem;
+  }
+`
 const Resume = () => {
+  const [y, setY] = useSpring(() => ({
+    y: window.scrollY,
+    onFrame: props => window.scroll(0, props.y),
+  }))
   return (
     <Page accent="blue" bgDesign="space" seoProfile="resume-page">
       <Header>
         <MeCard />
       </Header>
-      <ResumeSection>
-        <ResumeLayout>
-          <h2 className="title">Work Experience</h2>
-          <div className="content">
-            {resume["work-experience"].map((jobInfo, i) => (
-              <WorkCard key={i} {...jobInfo} />
-            ))}
-          </div>
-        </ResumeLayout>
-      </ResumeSection>
+      <PageWrapper>
+        <div>
+          <Scroller>
+            <h2>BOOYA</h2>
+            <animated.button
+              type="button"
+              onClick={() =>
+                setY({
+                  from: { y: 100 },
+                  to: { y: window.scrollY },
+                  onStart: () => console.log("test"),
+                })
+              }
+            >
+              {y.y}
+            </animated.button>
+          </Scroller>
+        </div>
+        <ResumeSection>
+          <ResumeLayout>
+            <h2 className="title">Summary of Qualifications</h2>
+          </ResumeLayout>
+          <ResumeLayout>
+            <h2 className="title">Character Sheet</h2>
+          </ResumeLayout>
+          <ResumeLayout>
+            <h2 className="title">Education</h2>
+          </ResumeLayout>
+          <ResumeLayout>
+            <h2 className="title">Work Experience</h2>
+            <div className="content">
+              {resume["work-experience"].map((jobInfo, i) => (
+                <WorkCard key={i} {...jobInfo} />
+              ))}
+            </div>
+          </ResumeLayout>
+
+          <ResumeLayout>
+            <h2 className="title">Project Showcase</h2>
+          </ResumeLayout>
+          <ResumeLayout>
+            <h2 className="title">Volunteer Experience</h2>
+          </ResumeLayout>
+
+          <ResumeLayout>
+            <h2 className="title">Achievements</h2>
+          </ResumeLayout>
+          <ResumeLayout>
+            <h2 className="title">Interests</h2>
+          </ResumeLayout>
+        </ResumeSection>
+      </PageWrapper>
     </Page>
   )
 }
