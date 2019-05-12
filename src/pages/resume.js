0f89a22d28ea35ecc9b54be099f5ae0f6c270166
Leander Rodrigues <me@leander.xyz>
2019-05-12 12:31:40 -0400

    💄 Simplify nav bar

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -23,7 +23,7 @@ const ResumeSection = styled(Default)`
   margin: 2rem 0;
 `
 
-const ResumeLayout = styled(div)`
+const ResumeLayout = styled.div`
   display: grid;
   background: ${themer("shade.lightest")};
   border-radius: 2rem;
@@ -31,14 +31,14 @@ const ResumeLayout = styled(div)`
   max-width: ${themer("spacing.maxWidth")};
   margin: 2rem auto;
   .title {
-    padding: 2.5rem;
+    padding: ${themer("constants.navBarHeight")} 2.5rem;
     grid-area: 1 / 1 / 1 / 1;
     border-right: 2px solid ${themer("accent")};
     text-align: right;
     font-weight: 300;
   }
   .content {
-    padding: 2.5rem;
+    padding: ${themer("constants.navBarHeight")} 2.5rem;
     grid-area: 1 / 2 / 1 / 2;
     font-size: 1.5rem;
     text-align: left;
@@ -47,15 +47,38 @@ const ResumeLayout = styled(div)`
 
 const PageWrapper = styled.div`
   display: grid;
-  grid-template-columns: 150px auto;
+  grid-template-columns: 200px auto;
+  margin: 0 auto;
+  max-width: calc(${themer("spacing.maxWidth")} + 200px);
 `
 
 const Scroller = styled.aside`
-  position: sticky;
   color: ${themer("shade.lightest")};
-  top: ${themer("constants.navBarHeight")};
-  button {
-    padding: 2rem;
+  margin: 2rem 1rem;
+  .links {
+    position: sticky;
+    top: ${themer("constants.navBarHeight")};
+    margin: 0 auto;
+  }
+  .page-link {
+    display: block;
+    font-size: 1.5rem;
+    margin: 1.5rem 1rem;
+    margin-left: 2rem;
+    padding-left: 0.5rem;
+    font-weight: 300;
+    position: relative;
+    &:before {
+      content: "# ";
+      position: absolute;
+      right: 100%;
+      font-weight: bold;
+      top: 0;
+      color: ${themer("accent")};
+    }
+    &:hover {
+      text-decoration: underline ${themer("accent")};
+    }
   }
 `
 const Resume = () => {
@@ -65,12 +88,34 @@ const Resume = () => {
         <MeCard />
       </Header>
       <PageWrapper>
-        <div>
-          <Scroller>
-            <a href="#soq">Summary of Qualifications</a>
-            <a href="#cs">Character Sheet</a>
-          </Scroller>
-        </div>
+        <Scroller>
+          <div className="links">
+            <a className="page-link" href="#soq">
+              Summary of Qualifications
+            </a>
+            <a className="page-link" href="#cs">
+              Character Sheet
+            </a>
+            <a className="page-link" href="#edu">
+              Education
+            </a>
+            <a className="page-link" href="#we">
+              Work Experience
+            </a>
+            <a className="page-link" href="#ps">
+              Project Showcase
+            </a>
+            <a className="page-link" href="#ve">
+              Voluneer Experience
+            </a>
+            <a className="page-link" href="#ach">
+              Acheivements
+            </a>
+            <a className="page-link" href="#int">
+              Interests
+            </a>
+          </div>
+        </Scroller>
         <ResumeSection>
           <ResumeLayout>
             <h2 className="title" id="soq">
