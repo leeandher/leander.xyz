0538d7153e19ff8fc8b99a258698bcdaf7c02dbc
Leander Rodrigues <me@leander.xyz>
2019-09-20 03:36:25 -0400

    🍱 Update pictures!

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,21 +1,32 @@
 import React from "react"
 import styled from "styled-components"
+import { Link } from "gatsby"
 
 import Page from "../components/Page"
+import Button from "../components/Button"
 
 import { themer } from "../styles/helpers"
 
 const Header = styled.div`
   height: 100vh;
   text-align: center;
+  flex-direction: column;
   display: flex;
   align-items: center;
   justify-content: center;
   padding: ${themer("spacing.default")};
   color: ${themer("accent")};
+  margin-top: -${themer("constants.navBarHeight")};
   span {
     color: ${themer("shade.lighter")};
   }
+`
+
+const StylishHome = styled(Button)`
+  display: block;
+  color: ${themer("shade.lightest")};
+  font-size: 1.6rem;
+  font-weight: normal;
 `
 
 const Projects = () => {
@@ -24,10 +35,14 @@ const Projects = () => {
       <Header>
         <h1>
           <code>
-            {/* eslint-disable-line react/jsx-no-comment-textnodes */}
+            {/* eslint-disable */}
             // TODO: <span>Make better 404 page</span>
+            {/* eslint-enable */}
           </code>
         </h1>
+        <Link to="/">
+          <StylishHome>Take me home!</StylishHome>
+        </Link>
       </Header>
     </Page>
   )
