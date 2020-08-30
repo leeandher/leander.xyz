ce9b121e4a3b076c2f3f5d08fdf34ffc2258deea
Leander Rodrigues <me@leander.xyz>
2020-08-30 10:49:33 -0400

    :sparkles: Add edit button to note posts

--- src/templates/NoteTemplate.js
+++ src/templates/NoteTemplate.js
@@ -3,6 +3,7 @@ import { graphql } from "gatsby"
 import slugify from "slugify"
 
 import Page from "../components/Page"
+import Button from "../components/Button"
 import InnerLink from "../components/page-specific/Media/InnerLink"
 
 import {
@@ -11,16 +12,19 @@ import {
   MediaHeader,
   MediaSection,
   ScrollProgress,
+  MediaPostContent,
 } from "../components/page-specific/Media"
 
 const NoteTemplate = ({ data, pageContext }) => {
-  const { markdownRemark } = data
+  const { markdownRemark, site } = data
   const { html } = markdownRemark
-  const { relativeDirectory: parentCategory } = pageContext
+  const { absolutePath, relativeDirectory: parentCategory } = pageContext
   const parentCategorySlug = slugify(`${parentCategory}`, {
     lower: true,
   })
-
+  const editLinkLocation = `${
+    site.siteMetadata.repository
+  }/blob/live/${absolutePath.slice(absolutePath.indexOf("src/pages/notes"))}`
   return (
     <Page
       accentKey={parentCategorySlug}
@@ -37,6 +41,11 @@ const NoteTemplate = ({ data, pageContext }) => {
       <MediaSection>
         <ContentWrapper>
           <MediaContent dangerouslySetInnerHTML={{ __html: html }} />
+          <MediaPostContent>
+            <a href={editLinkLocation} target="_blank" rel="noreferrer">
+              <Button>📝 Edit this page</Button>
+            </a>
+          </MediaPostContent>
         </ContentWrapper>
       </MediaSection>
     </Page>
@@ -47,6 +56,11 @@ export default NoteTemplate
 
 export const noteQuery = graphql`
   query($absolutePath: String!) {
+    site {
+      siteMetadata {
+        repository
+      }
+    }
     markdownRemark(fileAbsolutePath: { eq: $absolutePath }) {
       html
     }
