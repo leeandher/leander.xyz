c37083c1e2141ef278b56c25957df8f07bbb840b
Leander Rodrigues <me@leander.xyz>
2025-04-03 08:23:41 -0400

    ➕ Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,13 @@ import svelte from "@astrojs/svelte";
 import netlify from "@astrojs/netlify";
 import sitemap from "@astrojs/sitemap";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), sitemap()],
   adapter: netlify(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
