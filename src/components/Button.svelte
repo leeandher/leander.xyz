899dead1adc69da41027a0629f9160e8a5f58953
Leander Rodrigues <me@leander.xyz>
2025-04-06 20:00:06 -0400

    🚧 Test out some svelte usage on astro

---
+++ src/components/Button.svelte
@@ -0,0 +1,13 @@
+<script>
+  let counter = $state(0);
+  function increment() {
+    counter += 1;
+  }
+</script>
+
+<button
+  onclick={increment}
+  class={["border-2", "rounded-sm", "block", "p-2", "bg-amber-400"]}
+>
+  {counter}
+</button>
