86e42804394e4e60becba1a0c337867cf5becc3d
Leander Rodrigues <me@leander.xyz>
2025-04-26 21:37:58 -0400

    🚧 WIP on the wares header

---
+++ src/components/WaresShowcase.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+</script>
+
+<section class="mx-4 h-screen flex flex-col justify-start">
+  <div class="whitespace-nowrap">
+    <div
+      class="font-fancy text-base text-5xl sm:text-7xl relative items-baseline grid md:grid-cols-[auto_1fr] gap-x-4 gap-y-1"
+    >
+      <span
+        class="before:content-['wares'] before:text-crackle absolute inset-0 -left-1 block -z-1"
+      ></span>
+      <h2 class="text-base">wares</h2>
+      <div
+        class="text-base/70 text-xl sm:text-2xl content-end flex-1 text-wrap relative pt-2"
+      >
+        <span class="bg-crackle rounded-full h-1 w-full absolute top-0 left-0"
+        ></span>
+        most people call them projects
+      </div>
+    </div>
+  </div>
+</section>
