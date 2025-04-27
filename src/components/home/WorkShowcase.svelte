060399f5c19841e8b05bb70420cc76074d312fea
Leander Rodrigues <me@leander.xyz>
2025-04-27 16:25:52 -0400

    🚧build out the work section

---
+++ src/components/home/WorkShowcase.svelte
@@ -0,0 +1,47 @@
+<section
+  class="h-[1000px] relative flex flex-col bg-surface-alt px-16 py-8 overflow-hidden"
+>
+  <div class="bg-stone-800 flex-1 mb-24 rounded-4xl flex gap-x-8 px-8">
+    <div class="bg-stone-700 my-8 flex-3/4 rounded-3xl"></div>
+    <div class=" my-8 flex-1/4 rounded-3xl">
+      <div class="justify-end gap-x-4">
+        <h2
+          class="font-techy text-5xl sm:text-7xl text-pop-100 relative z-1 tracking-widest"
+        >
+          <span
+            class="before:content-['WORK'] before:text-pop absolute inset-0 left-1 block -z-1"
+          ></span>
+          WORK
+        </h2>
+        <div
+          class="gap-y-1 sm:gap-y-1.5 flex justify-center items-end h-full transition-all gap-x-2"
+        >
+          <div class="text-pop-100 font-fancy text-2xl">9-2-5</div>
+          <div
+            class="flex flex-col gap-y-1 sm:gap-y-1.5 justify-center items-start h-full transition-all flex-1"
+          >
+            <div
+              aria-hidden="true"
+              class="bg-pop rounded-full h-1 w-8/10"
+            ></div>
+            <div
+              aria-hidden="true"
+              class="bg-pop rounded-full h-1 w-9/10"
+            ></div>
+            <div
+              aria-hidden="true"
+              class="bg-pop rounded-full h-1 w-10/10"
+            ></div>
+          </div>
+        </div>
+        <div
+          class="bg-[#8DD325] text-stone-900 font-['Times_New_Roman'] rounded-lg px-2 py-2 -rotate-6 ml-1 translate-y-8"
+        >
+          <span class="text-4xl">Y2K</span>
+          <span class="text-lg">Ready</span>
+          <div class="text-xs">(Please do not remove until 01/00)</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
