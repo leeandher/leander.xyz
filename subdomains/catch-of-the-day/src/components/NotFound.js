548f83a89dc68dd0e3d9be5e552002ba4d73f858
Leander Rodrigues <me@leander.xyz>
2025-05-17 13:11:42 -0400

    🔥 rm subdomains

--- subdomains/catch-of-the-day/src/components/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const NotFound = () => (
-  <div>
-    <h2>NotFound?!</h2>
-  </div>
-);
-
-export default NotFound;
--- subdomains/stacker-news/server/src/resolvers/User.js
+++
@@ -1,6 +0,0 @@
-// Resolves the 'links' field on User type in '../schema.graphql'//
-const links = ({ id }, args, context) => context.prisma.user({ id }).links();
-
-module.exports = {
-  links
-};
